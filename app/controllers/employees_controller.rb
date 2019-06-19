a9c1d354d67a0e82f65b536b3c0bc0f419a9d18a
Nikola Zatezalo <nikola@platform45.com>
2019-06-19 10:23:18 +0200

    Delete employee added

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -10,6 +10,13 @@ def new
   @employee = @division.employees.new
 end
 
+def destroy
+  @division = Division.find(params[:division_id])
+  @employee = @division.employees.find(params[:id])
+  @employee.destroy
+  redirect_to @division
+end
+
 private
   def employee_params
     params.require(:employee).permit(:employee_name)
